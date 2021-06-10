e603f157ac3f42e6ba5a752e0c2db740de237cff
deemak <deema_k@mail.ru>
2021-06-10 13:55:48 +0300

    actions
    Signed-off-by: deemak <deema_k@mail.ru>

--- cmd/distr/math.go
+++ cmd/distr/math.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"github.com/digital-technology-agency/math"
+	"log"
+)
 
+func main() {
+	n := *flag.Int64(`n`, 1000, ``)
+	k := *flag.Int64(`k`, 2, ``)
+	flag.Parse()
+	log.Printf(`Params N=%d K=%d`, n, k)
+	log.Printf(`Uint64 %d`, math.CnkUint(n, k))
+	log.Printf(`String %s`, math.CnkStr(n, k))
 }
